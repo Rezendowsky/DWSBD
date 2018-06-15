332bcb2b9c24d59264c74a25fd954b72f7148957
Giovani Paganini <giovanipaganini@outlook.com>
2018-06-15 16:41:13 -0300

    Adição - Adicionado base da tela da Recepcionista

---
+++ telas/tMenuRecepcionista.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="style.css" rel="stylesheet" type="text/css"/>
+        <title>Recepcionista</title>
+    </head>
+    <body>
+        <div id="sessao">
+            <table class="tableForm">                
+                <tr class="tableHeader">
+                    <th class="tdHeader" colspan="3">Recepcionista</th>
+                </tr>
+                <tr>
+                    <td><input class="btn btnMenu btnMenuNeutro" type="submit" value="Pesquisar Paciente" name="btnMenu"/></td>
+                    <td><input class="btn btn btnMenu btnMenuPositivo" type="submit" value="Registrar Paciente" name="btnMenu"/></td>
+                </tr>
+                <tr>
+                    <td><input class="btn btnMenu btnMenuNeutro" type="submit" value="Pesquisar Funcionário" name="btnMenu"/></td>
+                    <td><input class="btn btnMenu btnMenuPositivo" type="submit" value="Registrar Funcionario" name="btnMenu"/></td>
+                </tr>
+                <tr>
+                    <td><input class="btn btnMenu btnMenuNeutro" type="submit" value="Agendamentos" name="btnMenu"/></td>
+                    <td><input class="btn btnMenu btnMenuNeutro" type="submit" value="Tratamentos" name="btnMenu"/></td>
+                </tr>
+            </table>            
+        </div>        
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
